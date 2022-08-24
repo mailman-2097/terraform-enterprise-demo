f97e2a382a8c4accba42a2f5d0c4ba33d36ecd60
Nishad Saithaly <Nishad.Saithaly@health.nsw.gov.au>
2022-08-24 15:13:07 +1000

    Change bucket name

--- maint.tf
+++ maint.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "terraform-demo-bucket-20220824-1234567"
 
   versioning = {
     enabled = true
